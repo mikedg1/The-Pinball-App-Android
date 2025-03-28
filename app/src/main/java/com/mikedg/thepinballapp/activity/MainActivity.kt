ced4ff247e25840d8327cc3955013370f8cf1368
Mike DiGiovanni <vinblau@gmail.com>
2025-03-28 11:17:50 -0400

    D - Refactor MainActivity to fix class declaration and manifest.
    Corrected the MainActivity class declaration by deleting nested class.

--- app/src/main/java/com/mikedg/thepinballapp/activity/MainActivity.kt
+++ app/src/main/java/com/mikedg/thepinballapp/activity/MainActivity.kt
@@ -6,15 +6,13 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import com.mikedg.thepinballapp.features.home.view.HomeScreen
 
-class MainActivity {
-    class MainActivity : ComponentActivity() {
-        override fun onCreate(savedInstanceState: Bundle?) {
-            super.onCreate(savedInstanceState)
-            enableEdgeToEdge()
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
 
-            setContent {
-                HomeScreen()
-            }
+        setContent {
+            HomeScreen()
         }
     }
 }
