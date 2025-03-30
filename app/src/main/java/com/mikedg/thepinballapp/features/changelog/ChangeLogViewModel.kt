049bfb0dc09d8b87c71e1826df7a337a712c36a0
Mike DiGiovanni <vinblau@gmail.com>
2025-03-30 08:12:45 -0400

    D - Refactor ChangeLogViewModel to use Hilt for dependency injection
    Replaced manual dependency initialization with Hilt to inject OpdbApiService, improving code scalability and maintainability. Added @HiltViewModel annotation and updated constructor to utilize Hilt's injection mechanism. Removed legacy initialization of OpdbApiService.

--- app/src/main/java/com/mikedg/thepinballapp/features/changelog/ChangeLogViewModel.kt
+++ app/src/main/java/com/mikedg/thepinballapp/features/changelog/ChangeLogViewModel.kt
@@ -4,20 +4,20 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.mikedg.thepinballapp.data.model.ChangeLog
 import com.mikedg.thepinballapp.data.remote.OpdbApiService
+import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
+import javax.inject.Inject
 
-class ChangeLogViewModel : ViewModel() {
+@HiltViewModel
+class ChangeLogViewModel @Inject constructor(private val opdbApiService: OpdbApiService): ViewModel() {
     private val _changeLog = MutableStateFlow<List<ChangeLog>>(emptyList())
     val changeLog = _changeLog.asStateFlow()
 
-    private val opdbService =
-        OpdbApiService() // TODO: pass dependency in, just don't want to deal with the Factories now
-
     init {
         viewModelScope.launch {
-            _changeLog.value = opdbService.fetchChangeLogs()
+            _changeLog.value = opdbApiService.fetchChangeLogs()
         }
     }
 }
